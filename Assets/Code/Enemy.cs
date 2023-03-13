100dea0695a79e2cd6378c179cc8134f7c11074b
timenko <timenkosss@mail.ru>
2023-03-13 21:11:19 +1000

    xd

--- Assets/Code/Enemy.cs
+++ Assets/Code/Enemy.cs
@@ -8,9 +8,14 @@ public class Enemy : MonoBehaviour
     public int maxHealth = 100;
     public int currentHealth = 0;
     public Animator animator;
+    
+    public AudioSource dead;
+
     void Start()
     {
         currentHealth = maxHealth;
+        dead = GameObject.Find("dead").GetComponent<AudioSource>();
+
     }
 
     public void TakeDamage(int damage)
@@ -30,6 +35,8 @@ public class Enemy : MonoBehaviour
         animator.SetBool("isDead",true);
         GetComponent<Collider2D>().enabled = false;
         this.enabled = false;
+        dead.Play();
+
 
     }
 
